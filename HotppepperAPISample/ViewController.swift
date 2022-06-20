a01a0c08ef04e20f776dfbb260024a45d1212a90
ShoheiKuwata <shouhei.k224k@keio.jp>
2022-06-20 18:33:07 +0900

    Initial Commit

---
+++ HotppepperAPISample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotppepperAPISample
+//
+//  Created by 桑田翔平 on 2022/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
